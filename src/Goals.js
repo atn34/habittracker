1b1c57642f9ed6eb881c29c61fa2f468daa621b3
Andrew Noyes <anoyes34@gmail.com>
2017-06-21 15:45:15 -0700

    Expunge tabs

--- src/Goals.js
+++ src/Goals.js
@@ -2,31 +2,31 @@ import React from 'react';
 import './Goals.css'
 
 const Goal = ({ goal, handleGoalClick }) => {
-	const now = new Date();
-	const lastDone = new Date(goal.get("lastDone"));
-	const doneToday = now.getDate() === lastDone.getDate()
-		&& now.getMonth() === lastDone.getMonth() && now.getFullYear() === lastDone.getFullYear();
-	return (
-		<div>
-			<button type="button" className="Goal-Button" onClick={() => (handleGoalClick(goal))}>{goal.get("name")}</button>
-			{"  "} 
-			{`state: ${goal.get("state")}`}
-			{"  "} 
-			{`doneToday: ${doneToday}`}
-			{"  "} 
-			{`lastDone: ${goal.get("lastDone")}`}
-		</div>
-		
-	);
+    const now = new Date();
+    const lastDone = new Date(goal.get("lastDone"));
+    const doneToday = now.getDate() === lastDone.getDate()
+        && now.getMonth() === lastDone.getMonth() && now.getFullYear() === lastDone.getFullYear();
+    return (
+        <div>
+            <button type="button" className="Goal-Button" onClick={() => (handleGoalClick(goal))}>{goal.get("name")}</button>
+            {"  "} 
+            {`state: ${goal.get("state")}`}
+            {"  "} 
+            {`doneToday: ${doneToday}`}
+            {"  "} 
+            {`lastDone: ${goal.get("lastDone")}`}
+        </div>
+        
+    );
 };
 
 const Goals = ({ goals, handleGoalClick }) => {
-	return (
-		<div className="Goals">
-			<h2>Goals</h2>
-			{goals.map(goal => <Goal goal={goal} key={goal.get("goalid")} handleGoalClick={handleGoalClick} />)}
-		</div>
-	);
+    return (
+        <div className="Goals">
+            <h2>Goals</h2>
+            {goals.map(goal => <Goal goal={goal} key={goal.get("goalid")} handleGoalClick={handleGoalClick} />)}
+        </div>
+    );
 }
 
 export default Goals;
