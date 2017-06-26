eb14198f1766bb94bb07f0c81793b38d6035e634
Andrew Noyes <anoyes34@gmail.com>
2017-06-26 11:39:44 -0700

    Switch to TODO/Done tables

--- src/Goals.js
+++ src/Goals.js
@@ -1,30 +1,47 @@
 import React from 'react';
 import './Goals.css'
 
-const Goal = ({ goal, handleGoalClick }) => {
-    const now = new Date();
+const DoneToday = (goal, now) => {
     const lastDone = new Date(goal.get("lastDone"));
-    const doneToday = now.getDate() === lastDone.getDate()
+    return now.getDate() === lastDone.getDate()
         && now.getMonth() === lastDone.getMonth() && now.getFullYear() === lastDone.getFullYear();
+};
+
+const Goal = ({ goal, doneToday, handleGoalClick }) => {
     return (
-        <div>
-            <button type="button" className="Goal-Button" onClick={() => (handleGoalClick(goal))}>{goal.get("name")}</button>
-            {"  "} 
-            {`state: ${goal.get("state")}`}
-            {"  "} 
-            {`doneToday: ${doneToday}`}
-            {"  "} 
-            {`lastDone: ${goal.get("lastDone")}`}
-        </div>
-        
+        <tr>
+            { !doneToday &&
+                <td><button type="button" className="Goal-Button" onClick={() => (handleGoalClick(goal))}>Mark Done</button></td>
+            }
+            <td><b>{goal.get("name")}</b></td>
+            <td>{`state: ${goal.get("state")}`}</td>
+            <td>{`lastDone: ${goal.get("lastDone")}`}</td>
+        </tr>
     );
 };
 
 const Goals = ({ goals, handleGoalClick }) => {
+    const now = new Date();
     return (
         <div className="Goals">
-            <h2>Goals</h2>
-            {goals.map(goal => <Goal goal={goal} key={goal.get("goalid")} handleGoalClick={handleGoalClick} />)}
+            <h2>TODO</h2>
+            <table className="Goals">
+                <tbody>
+                    {goals
+                        .filter(goal => !DoneToday(goal, now))
+                        .map(goal => <Goal goal={goal} key={goal.get("goalid")} doneToday={false} handleGoalClick={handleGoalClick} />)
+                    }
+                </tbody>
+            </table>
+            <h2>Done</h2>
+            <table className="Goals">
+                <tbody>
+                    {goals
+                        .filter(goal => DoneToday(goal, now))
+                        .map(goal => <Goal goal={goal} key={goal.get("goalid")} doneToday={true} handleGoalClick={handleGoalClick} />)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
